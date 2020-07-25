95056cf7c49e6a9322ae02773360b1e6bb5ce841
jimrothstein <jimrothstein@gmail.com>
2020-07-25 15:07:12 -0700

    add help file for pkg

---
+++ R/001_help_file.R
@@ -0,0 +1,16 @@
+#' jimTools: A package for odds & ends I have found useful.
+#'
+#' The foo package provides three categories of important functions:
+#' foo, bar and baz.
+#' 
+#' @section Foo functions:
+#' The foo functions ...
+#'
+#' @section To Use:
+#' ?jimTools  at the command line.
+#'
+#' @docType package
+#' @name jimTools
+NULL
+#> NULL
+
