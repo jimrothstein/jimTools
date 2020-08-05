8a2a4785ec8707b2cedc3e880479e6b418ad927f
jimrothstein <jimrothstein@gmail.com>
2020-08-04 20:00:43 -0700

    wip

--- R/compress.R
+++ R/compress.R
@@ -6,10 +6,22 @@
 
 #'  
 jr_compress_dir  <- function(dir) {
-out  <- paste0( lubridate::now(), 
-								"_archive_",
-								"compressed_files.tar.gz"
-		)
+	x  <- lubridate::now()
+
+	# format, lubridate  stringr
+	prefix  <- paste(
+				 	lubridate::year(x),
+			  	lubridate::month(x, label=TRUE, abbr=TRUE ),
+					stringr::str_pad(lubridate::day(x), 
+													width=2, side=c("left"), pad="0"),
+					lubridate::hour(x),
+					lubridate::minute(x), 
+					sep="_")
+
+	out  <- paste0(prefix , 
+									"_ARCHIVE_",
+									"compressed_files.tar.gz"
+			)
 
 	tar( tarfile =
 			here("compressed", out),
