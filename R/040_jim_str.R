9cf748b446a3bc84155c95e43f88572c0bd5739b
jimrothstein <jimrothstein@gmail.com>
2020-11-13 19:52:02 -0800

    wip

---
+++ R/040_jim_str.R
@@ -0,0 +1,19 @@
+#' @title jim_str() show str() information
+#'
+#' @description friendly version of str()
+#'
+#' @param X  object
+#' @param max.level number of levels
+#' @param vec.len number of  elements to show at each level
+#' 
+#' @return 
+#' @export
+jim_str  <- function(X=NULL, max.level =1 , vec.len =1 ){
+    if (is.null(X) ) print("help:   X, max.level, vec.len")
+    cat("For help, jim_str() \n")
+    str(X, max.level, vec.len)
+   }
+
+
+
+
