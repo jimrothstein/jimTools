e49a69d578419e8319d79c5151368a06f02b3b74
jimrothstein <jimrothstein@gmail.com>
2020-11-29 09:57:20 -0800

    wip

--- R/001_help_file.R
+++ R/001_help_file.R
@@ -1,13 +1,15 @@
 #' jimTools: A package for odds & ends I have found useful.
 #'
-#' The foo package provides three categories of important functions:
-#' foo, bar and baz.
+#' jimTools: Package for functions and info I find useful. 
+#'
 #' 
 #' @section Foo functions:
 #' The foo functions ...
 #'
 #' @section To Use:
 #' ?jimTools  at the command line.
+#' ls(asNamespace("jimTools")) - returns chr[] of functions
+#' lsf.str("package:jimTools") - list functions
 #'
 #' @docType package
 #' @name jimTools
