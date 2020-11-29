eed32366dd375cd336aabf85509adb05656d1bb6
jimrothstein <jimrothstein@gmail.com>
2020-11-29 09:26:36 -0800

    wip

---
+++ R/100_show_env.R
@@ -0,0 +1,9 @@
+#' show_env
+#' @export
+show_env <- function(){
+  list(ran.in = environment(),     # temporary (active, or runtime) env
+    parent = parent.env(environment()),# parent 
+    objects = ls.str(environment())
+  )
+}
+show_env()
