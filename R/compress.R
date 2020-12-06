071746ac43566954c121e3d9ded8e841cc8950e7
jimrothstein <jimrothstein@gmail.com>
2020-12-05 20:31:39 -0800

    second version of compressed

--- R/compress.R
+++ R/compress.R
@@ -1,13 +1,65 @@
 #'  compress a dir
 #' 
-#'  @param dir directory (under projet root) to compress
+#'  @description compress_dir()  creates .tar.gz file
+#'    if type is system, zipped file will be placed in toGoogle dir
+#'    if type is project, zipped file will be placed in compressed folder in
+#'    this package.
+#'  @param type project, if dir is inside project; 
+#'              system, if the_dirs is list of complete diretory paths
+#'  @param dir directory 
 #'  @return 
 #'  @export
+#'
+compress_dir  <- function(type = c("project", "system"), 
+                                      dir =NULL)
+{
+  x  <- lubridate::now()
+  cat("Processing.... ",dir, "\n")
+	prefix  <- paste(
+				 	lubridate::year(x),
+			  	lubridate::month(x, label=TRUE, abbr=TRUE ),
+					stringr::str_pad(lubridate::day(x), 
+													width=2, side=c("left"), pad="0"),
+					lubridate::hour(x),
+					lubridate::minute(x), 
+					#lubridate::second(x),
+					sep="_")
 
-#'  
-jr_compress_dir  <- function(dir) {
-	x  <- lubridate::now()
-  cat("Processing.... ",dir, "/n")
+	out_file  <- paste0(prefix , 
+									"_ARCHIVE_",
+                  basename(dir),
+									".tar.gz"
+			)
+  if (type == "project")
+	  tar( 	tarfile = here("compressed", out_file),
+				files=here(dir),  
+				compression = c("gzip"))
+  else {
+    
+    output_dir = "~/Downloads/toGoogle"
+    tar( tarfile = paste0(output_dir, "/", out_file),
+        files = dir,
+        compression = c("gzip")
+        )
+  }
+
+}
+
+#'
+#'
+#'
+#'
+#' REMVEVE
+jr_compress_dir  <- function(type = c("project", "system"), 
+# ============================================================
+                                      dir =NULL, 
+                                      the_dirs = NULL) 
+{
+  if (is.null(dir) && is.null(the_dirs)) 
+    stop("Please specify type, and one of `dir` or `the_dirs`")
+
+  x  <- lubridate::now()
+  cat("Processing.... ",dir, "\n")
 	# format, lubridate  stringr
 	prefix  <- paste(
 				 	lubridate::year(x),
@@ -23,9 +75,12 @@ jr_compress_dir  <- function(dir) {
 									"_ARCHIVE_",
 									"compressed_files.tar.gz"
 			)
-
-	tar( 	tarfile = here("compressed", out),
+  if (type == "project")
+	  tar( 	tarfile = here("compressed", out),
 				files=here(dir),  
 				compression = c("gzip"))
+  else
+    cat("wip ....", "\n")
+
 }
 
