a0da078efacdeacaf2afdd0f6ca8817f4ed4778d
jimrothstein <jimrothstein@gmail.com>
2023-03-15 21:45:02 -0700

    wip -  adding gh clone repo to an Rscript

---
+++ R/220_github_clone.R
@@ -0,0 +1,6 @@
+
+Rscript -e "system2('gh', args=c('repo clone', 'jimrothstein/jimTools', '~/code/jimTools/'))"
+##  PURPOSE:  clone reoo
+##
+##
+system2('ghi'):
--- Rscripts/gh_clone_repo.R
+++ Rscripts/gh_clone_repo.R
@@ -1,2 +1,28 @@
-#   NOTE:   final directory (~/code/jimTools/) is location to install
-Rscript -e "system2('gh', args=c('repo clone', 'jimrothstein/jimTools', '~/code/jimTools/'))"
+#!/usr/bin/env Rscript
+
+
+#     This is .R script
+#
+#
+# PURPOSE:   clone repo
+# USAGE:    ./gh_clone_repo.R   # at command line
+# Works!
+#
+
+
+#
+{
+  begin <- Sys.time()
+  pkg <- "lua_project"
+
+  system2("gh",
+    args = c(
+      "repo clone",
+      paste0("jimrothstein/", pkg),
+      paste0("~/code/", pkg)
+    )
+  )
+  end <- Sys.time()
+  diff <- end - begin
+  diff
+}
