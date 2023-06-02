df4f6b4a88e344e49808cfebed6247d97a9832c1
jimrothstein <jimrothstein@gmail.com>
2023-06-01 23:19:17 -0700

    wip

--- Rscripts/update_old_packages.R
+++ Rscripts/update_old_packages.R
@@ -1,25 +1,30 @@
 #!/usr/bin/env Rscript
 #
+# TODO   
+#   1JUNE2023
+#   -    more testing, not convinced this works with  matrix, boot, and others that refused to update easily (permisisons)
+#   -   move Rscripts to dotfiles??
 #
-# PURPOSE:   install  old R packages
+# PURPOSE:   find and upgrade  old R packages
 # USAGE:    ./update_old_packages.R  at COMMAND LIME
-# Works!
 #
+##     -   There is a problem with R trying to install in R_HOME, which only sudo has rights.
+##     -   Installing to ~/R/x86_64-pc-linux-gnu-library/4.x/  works, but not sure how to specifiy
+##     -   update.packages downloads and installs.   old.packages() only finds.
 
-
-# old.packages() finds packages with later version
-# OR, if built with earlier version of R
-#,
+# ,
 {
-  begin  <- Sys.time()
-  update.packages(
-                 ask = F,
-                oldPkgs = old.packages(),
-                checkBuilt=T)
+    begin <- Sys.time()
+    update.packages(
+        lib.loc = .libPaths()[[1]]   # ~/R/x86_64-pc-linux-gnu-library/4.x/
+        ask = F,
+        oldPkgs = old.packages(),
+        checkBuilt = T
+    )
 
-  end  <- Sys.time()
-  diff  <- end - begin
-  diff
+    end <- Sys.time()
+    diff <- end - begin
+    diff
 }
 
 
