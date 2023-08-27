e2e279a69175460c622d0e351c6b05537aa74930
jimrothstein <jimrothstein@gmail.com>
2023-08-26 23:49:40 -0700

    wip

---
+++ Rscripts/all_repos.R
@@ -0,0 +1,24 @@
+#   repos used for daily_git_pull.R and daily_git_push.R
+x <- c(
+  "/home/jim/bin",
+  "/home/jim/code/code_publish",
+  "/home/jim/code/code_racket",
+  "/home/jim/code/docs",
+  "/home/jim/code/jimTools",
+  "/home/jim/code/make_project",
+  #    "/home/jim/code/lmlcr",
+  "/home/jim/code/lua_project",
+  "/home/jim/code/nvim_project",
+  # "/home/jim/code/plugins", # my lua plugins
+  "/home/jim/code/SHINY",
+  "/home/jim/code/TAGS",
+  "/home/jim/code/health_labs",
+  "/home/jim/code/make_project",
+  "/home/jim/code/mp3/",
+  "/home/jim/dotfiles",
+  "/home/jim/.config/nvim/", ## for lazyvim config
+  "/home/jim/.config/kickstart/", ## for kickstart
+  "/home/jim/code/try_things_here",
+  "/home/jim/code/quarto_experiments/",
+  "/home/jim/code/zsh_project"
+)
--- Rscripts/daily_git_push.R
+++ Rscripts/daily_git_push.R
@@ -14,30 +14,7 @@ library(tinytest)
 #
 
 #     "/home/jim/code/youtube_api",
-x <- c(
-  "/home/jim/bin",
-  "/home/jim/code/code_publish",
-  "/home/jim/code/code_racket",
-  "/home/jim/code/docs",
-  "/home/jim/code/jimTools",
-  "/home/jim/code/make_project",
-  #    "/home/jim/code/lmlcr",
-  "/home/jim/code/lua_project",
-  "/home/jim/code/nvim_project",
-  # "/home/jim/code/plugins", # my lua plugins
-  "/home/jim/code/SHINY",
-  "/home/jim/code/TAGS",
-  "/home/jim/code/health_labs",
-  "/home/jim/code/make_project",
-  "/home/jim/code/mp3/",
-  "/home/jim/dotfiles",
-  "/home/jim/.config/nvim/", ## for lazyvim config
-  "/home/jim/.config/kickstart/", ## for kickstart
-  "/home/jim/code/try_things_here",
-  "/home/jim/code/quarto_experiments/",
-  "/home/jim/code/zsh_project"
-)
-
+source("~/code/jimTools/Rscripts/all_repos.R")
 # 	---------------------
 add_commit_push <- function(dir = NULL) {
   if (is.null(dir) || is.na(dir) || nchar(dir) == 0) {
