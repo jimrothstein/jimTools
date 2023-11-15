ac6205e194075aa2177f7c5d0649130923f764c5
jimrothstein <jimrothstein@gmail.com>
2023-11-15 12:18:06 -0800

    wip

--- R/202_WIP_Rscript.R
+++ R/202_WIP_Rscript.R
@@ -2,12 +2,15 @@
 
 # PURPOSE:   find and upgrade  R Packages
 # Rscript version of 202_, which should be deleted when this works
-# USAGE:  ./202_
+# USAGE:  ./202_ (run at CLI, as Rscript)
 #
-# TODOP (2023-11-06)
+# TODO:
+# 2023-11-13
+#   -   continue to verify workinga
+#   -   now logging
+# 2023-11-06
 #   -   add quiet=   to updates
 #   -   add logging? log4j
-#   -   legacy - removed
 # TODO: (2023-11-03)
 #   -   Confirm working Rscript
 #
@@ -39,52 +42,48 @@
 #
 
 ### currently installed pkgs , 266 (with dup)
-cat("begin")
+library(log4r)
 library(data.table)
-dt <- as.data.table(installed.packages())
-head(dt)[1:3, 1:3]
-dim(dt) # [1] 264  16
 
+logger <- logger()
+info(logger, "Start ... ")
 
-
-
-# REF: https://medium.com/analytics-vidhya/r-data-table-joins-48f00b46ce29
-# Notation:
-#   on=.(x,y)
-#   on=.(x < 5, y> y)  non-equi join
-#   on=.(...), z := ...   adds column (in reference, no copy)
 
 ### candidates
+info(logger, "Finding ... candidates")
+dt <- as.data.table(installed.packages())
 cand <- as.data.table(old.packages(checkBuilt = T))
-dim(cand)
-
 ### Any candidate in lib1, then update these.
-cat("begin lib1")
 lib2 <- .libPaths()[[2]]
 lib1 <- .libPaths()[[1]]
-lib1
-lib2
-cand[LibPath == .libPaths()[[1]], ]
-update.packages(
-    libinst = lib1,
-    ask = FALSE,
-    oldPkgs = as.matrix(cand[LibPath == lib1, ]),
-    quiet = TRUE
-)
-
+x <- cand[LibPath == .libPaths()[[1]], ]
+if (nrow(x) == 0) {
+    info(logger, "Lib1 has no candidates")
+} else {
+    info(logger, "Updating ... lib1 candidates")
+    update.packages(
+        libinst = lib1,
+        ask = FALSE,
+        oldPkgs = as.matrix(cand[LibPath == lib1, ]),
+        quiet = TRUE
+    )
+}
+info(logger, "Done with lib1  ... ")
 
 ### Check lib2 for valid candidates:
 # if cand is in lib1, not need to check if 2 (SEE TOY)
 # but if cand in 2, only need to check if was every installed in 1
-cand
-cand[LibPath == lib2, ][!dt[LibPath == lib1, ], on = .(Package)]
 x <- cand[LibPath == lib2, ][!dt[LibPath == lib1, ], on = .(Package)]
-x
-update.packages(
-    instlib = lib1,
-    ask = FALSE,
-    oldPkgs = as.matrix(x),
-    quiet = TRUE
-)
 
-cat("done\n")
+if (nrow(x) == 0) {
+    info(logger, "Lib2 has no update candidates ... ... ")
+} else {
+    info(logger, "Updating Lib2 candidates ... ... ")
+    update.packages(
+        instlib = lib1,
+        ask = FALSE,
+        oldPkgs = as.matrix(x),
+        quiet = TRUE
+    )
+}
+info(logger, "Lib1 and Lib2 are done ...")
