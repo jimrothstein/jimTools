692d7094a930f83161f40782d25cde740787a8a6
jimrothstein <jimrothstein@gmail.com>
2026-06-19 12:00:28 -0700

    wip

--- R/350_daily_git_push.R
+++ R/350_daily_git_push.R
@@ -2,10 +2,6 @@
 
 # daily_git_push.R
 #
-# 	TODO
-# 	  - add_commit_push  clean up if statements
-#         - ~/git_log.log  ADD dates, more info
-
 # PURPOSE:  update all github repos (SEE list: all_repos.R)
 # USAGE:
 
@@ -15,42 +11,8 @@ get_dirs <- function(base_dir) {
   dirs <- list.dirs(base_dir, full.names = TRUE, recursive = FALSE)
   git_dirs <- dirs[dir.exists(file.path(dirs, ".git"))]
 
-# DIR, to exclude
+  # DIR, to exclude
   git_dirs = git_dirs[git_dirs != "/home/jim/code/throwaway"]
-}
-
-add_commit_push <- function(dir = NULL) {
-
-  # stop if evaluates to F
-  stopifnot(
-    !is.na(dir) && dir.exists(dir)
-  )
-  old <- setwd(dir)
-  system2("git", args = c("add", "."))
-  #  ====
-  #    ❌ ERROR: Push to feature-branch failed!"
-
-  system2("git", args = c(paste0("commit -m ", "wip", " >>", " ~/git_log.log")))
-
-  tryCatch(
-    {
-      system2("git", args = c("push", "--quiet"))
-    },
-    error = function(e) {
-      cat("Error in git push for directory:", dir, "\n")
-      cat("Error message:", e$message, "\n")
-    }
-  )
-
-  cat("---------------------------------\n")
-  cat("Start: push ....", dir, "\n")
-  cat("---------------------------------\n")
-
-  setwd(old)
-  if (FALSE) {
-    system2("git", args = c("status"))
-    getwd()
-  }
 }
 
 system2(
@@ -62,11 +24,40 @@ system2(
   )
 )
 
+# one dir at a time
+add_commit_push <- function(dir = NULL) {
+  # stop if evaluates to F
+  stopifnot(
+    !is.na(dir) && dir.exists(dir)
+  )
+  old <- setwd(dir)
+  message(paste("Processing:", dir))
+  
+  system2("git", args = c("add", "."))
 
-lapply(git_dirs, add_commit_push)
+  #  ====
+  #    ❌ ERROR: Push to feature-branch failed!"
 
-#apply(git_dirs, add_commit_push, FUN.VALUE = FALSE)
-#invisible(vapply(git_dirs, add_commit_push, FUN.VALUE = FALSE))
+  system2("git", args = c(paste0("commit -m ", "wip", " >>", " ~/git_log.log")))
+
+  tryCatch(
+    {
+      system2("git", args = c("push", "--quiet"))
+      return(TRUE)
+    },
+    error = function(e) {
+      cat("Error in git push for directory:", dir, "\n")
+      cat("Error message:", e$message, "\n")
+      return(FALSE)
+    }
+  )
+
+  setwd(old)
+}
+
+
+
+result = lapply(git_dirs, add_commit_push, FUN.VALUE = TRUE)
 
 if (FALSE) {
   vapply(x, add_commit_push, FUN.VALUE = FALSE)
